95eb9cd0216c3060cac4ab69b2e8442090387c15
Amina201202 <amina.isayeva2015@gmail.com>
2025-09-16 19:32:20 +0400

    Script.js

---
+++ Ders-71/script.js
@@ -0,0 +1,6 @@
+function showMessage(){
+    document.getElementById("displayId").innerHTML="Hello World!"
+    document.getElementById("displayId").innerText="Hello World!"
+    document.getElementById("displayId").textContent="Hello World!"
+    document.getElementById("nameId").value="Hello World!"
+}
