2a6be469d8dfe799f229d404a8e2400fce3a68cb
Amina201202 <amina.isayeva2015@gmail.com>
2025-09-19 17:34:46 +0400

    Ders-72

---
+++ Ders-72/script.js
@@ -0,0 +1,13 @@
+function fillphoto(){
+    document.getElementById("photoId").src="nature2.jpeg"
+}
+
+
+function addDoc(){
+    document.write("<button value='idk' style='width:100px; height=40px;'>Hello World!!</button>")
+}
+
+
+function showMessage(){
+    document.getElementById("displayID").innerHTML=prompt("Say something!!");
+}
